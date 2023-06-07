5c27da986203cb7dae94b63b6bd9f42c41940dff
João Capela <pg38274@alunos.uminho.pt>
2023-06-07 18:51:11 +0100

    just to demonstrate

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -6,3 +6,6 @@ from ci_cd.hello_world import HelloWorld
 class TestHelloWorld(TestCase):
     def test_hello_world(self):
         self.assertEqual(HelloWorld(name="Joao").say_hello(), "Hello Joao!")
+
+    def test_hello_world2(self):
+        self.assertEqual(HelloWorld(name="Joao").say_hello(), "Hello Joao!")
