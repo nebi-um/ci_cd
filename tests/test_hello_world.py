fe43b4d3d5579e423f4275d3ff9d3a50b4f5a63f
id9411@alunos.uminho.pt <id9411@alunos.uminho.pt>
2024-11-04 10:20:27 +0000

    [ADD] add test

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -9,3 +9,6 @@ class TestHelloWorld(TestCase):
 
     def test_hello_world2(self):
         self.assertEqual(HelloWorld(name="Joao").say_hello(), "Hello Joao!")
+
+    def test_hello_world3(self):
+        self.assertEqual(HelloWorld(name="Joao").say_hello(), "Hello Joao!")
